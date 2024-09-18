36ec65a779939cead30d82b7c40b78d3a6204520
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-09-18 13:50:10 +0530

    Loops

---
+++ Level 2/Loops.js
@@ -0,0 +1,15 @@
+https://www.interviewbit.com/problems/js-loops/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = parseInt(readLine().trim());
+// YOUR CODE GOES HERE
+
+for (var i=0; i <= num; i++){
+    
+    if (i % 2 == 1){
+        
+        console.log(i);
+    }
+    
+}
