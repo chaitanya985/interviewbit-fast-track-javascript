2f29684ee8b359817a686802aa1eeccffe8ca1fe
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-09-18 13:46:59 +0530

    Comparision Operation & If-Else

---
+++ Level 2/Comparision Operation & If-Else.js
@@ -0,0 +1,30 @@
+https://www.interviewbit.com/problems/js-comparision-operation-if-else/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = readLine().trim();
+// YOUR CODE GOES HERE
+
+if (num >= 90){
+    
+    console.log("A");
+}
+if (num >= 80){
+    
+    console.log("B");
+}
+if (num >= 70){
+    
+    console.log("C");
+}
+if (num >= 60){
+    
+    console.log("D");
+}
+if (num >= 50){
+    
+    console.log("E");
+}
+else{
+    console.log("F");
+}
