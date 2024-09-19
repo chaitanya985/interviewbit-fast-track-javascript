8e5b1d980e66d56e4c1dce6c36980e251ee0c231
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-09-19 13:29:52 +0530

    String Methods

---
+++ Level 4/String Methods.js
@@ -0,0 +1,9 @@
+https://www.interviewbit.com/problems/string-methods/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num value is in variable *num*
+var num = readLine().trim();
+// YOUR CODE GOES HERE
+var res=num.toUpperCase();
+
+console.log(num+res);
