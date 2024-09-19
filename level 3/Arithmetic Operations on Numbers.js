3aa1d257fce35ccc590bf3b80e7fdf8ab2bcbac8
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-09-19 13:25:55 +0530

    Arithmetic Operations on Numbers

---
+++ level 3/Arithmetic Operations on Numbers.js
@@ -0,0 +1,10 @@
+https://www.interviewbit.com/problems/arithmetic-operations-on-numbers/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = parseInt(readLine().trim());
+// YOUR CODE GOES HERE
+
+var res=num * num + num * num * num - num;
+
+console.log(res);
