1249ebbb4cb6ecae04b0c69a540fa1384d74c4ae
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-09-19 13:26:52 +0530

    Math methods in JS

---
+++ level 3/Math methods in JS.js
@@ -0,0 +1,9 @@
+https://www.interviewbit.com/problems/math-methods-in-js/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = parseInt(readLine().trim());
+// YOUR CODE GOES HERE
+var res=Math.round(Math.log2(num));
+
+console.log(res);
