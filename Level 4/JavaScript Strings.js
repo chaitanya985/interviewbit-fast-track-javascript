6a60f1cf258803d1fd3954a83b08ad1b3a2b5a66
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-09-19 13:29:06 +0530

    JavaScript Strings

---
+++ Level 4/JavaScript Strings.js
@@ -0,0 +1,8 @@
+https://www.interviewbit.com/problems/javascript-strings/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num value is in variable *num*
+var num = readLine().trim();
+// YOUR CODE GOES HERE
+
+console.log(num+num.length);
